ed91722e02d20041ce8d268e03ae74b865c59161
Shuntian Liu <shuntian.liu@outlook.com>
2018-06-26 15:00:29 +0100

    small changes

--- test.py
+++ test.py
@@ -1,16 +1,36 @@
-class A(object):
-    def __init__(self):
-        print("a")
+#background_image_filename = 'sushiplate.jpg'
+sprite_image_filename = 'fugu.png'
 
-class B(object):
-    def __init__(self):
-        print('b')
+import pygame
+from pygame.locals import *
+from sys import exit
 
-class C(A,B):
-    def __init__(self):
-        super().__init__()
-        A().__init__()
+pygame.init()
 
+screen = pygame.display.set_mode((1000,1000), 0, 32)
+clock=pygame.time.Clock()
 
+#background = pygame.image.load(background_image_filename).convert()
+sprite = pygame.image.load("/Users/liuliu/myDocuments/Python/pygame_project/generic_platformer_tiles.png").convert()
+# sprite的起始x坐标
+x = 0.
 
-n=C()
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    screen.fill((0,0,0))
+   # screen.blit(background, (0, 0))
+    time_passed=clock.tick()/1000.0
+    print(time_passed)
+    distance_moved=time_passed*250
+
+    screen.blit(sprite, (x, 100))
+    x += distance_moved  # 如果你的机器性能太好以至于看不清，可以把这个数字改小一些
+
+    # 如果移动出屏幕了，就搬到开始位置继续
+    if x > 1000.:
+        x -= 1000.
+
+    pygame.display.update()
