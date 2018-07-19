c0a6b854116bbec70fe1a94197d2a5aafb1e27d6
Vincent-lau <guang243683484@163.com>
2018-07-19 21:14:30 +0700

    player blockde by the wall

--- edu_game.py
+++ edu_game.py
@@ -7,6 +7,58 @@ DARKGREY=(169,169,169)
 RED = (255, 0, 0)
 BLUE=(0,0,255)
 
+class Player(pg.sprite.Sprite):
+    def __init__(self,pos,size,color):
+        super().__init__()
+        self.image=pg.Surface(size)
+        self.rect=self.image.get_rect(topleft=pos)
+        self.image.fill(color)
+        self.time=0
+        self.cor=[0,0]      # cor[0] is the row number and cor[1] is the column number
+
+
+    def tracking_key(self,keys):
+        if keys==pg.K_RIGHT:
+            self.move([self.rect.x+50,self.rect.y])
+        elif keys==pg.K_LEFT:
+            self.move([self.rect.x-50,self.rect.y])
+        elif keys==pg.K_UP:
+            self.move([self.rect.x,self.rect.y-50])
+        elif keys==pg.K_DOWN:
+            self.move([self.rect.x,self.rect.y+50])
+
+    def move(self,endPos):
+        endX=endPos[0]
+        endY=endPos[1]
+        dirX = endX - self.rect.x
+        dirY = endY - self.rect.y
+        newR = self.cor[0]
+        newC = self.cor[1]
+
+
+        if dirX > 0:
+            newC=self.cor[1] + 1
+        elif dirX < 0:
+            newC=self.cor[1] - 1
+
+        if dirY > 0:
+            newR=self.cor[0] + 1
+        elif dirY<0:
+            newR=self.cor[0] - 1
+
+
+        flag = (maze[newR][newC]==0)
+        print(flag)
+
+        if flag:
+            if dirX:
+                self.rect.move_ip(dirX , 0)
+                self.cor[1]=newC
+            if dirY:
+                self.rect.move_ip(0 , dirY)
+                self.cor[0]=newR
+        print(self.cor)
+
 
 class Tile(pg.sprite.Sprite):
     def __init__(self,pos,size,color):
@@ -60,13 +112,17 @@ for i in range(10):
             tile_group.add(tile)
             all_sprites_group.add(tile)
 
+myPlayer=Player([15,15],[20,20],BLUE)
+all_sprites_group.add(myPlayer)
 
 while not done:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done=True
+        elif event.type == pg.KEYDOWN:
+            myPlayer.tracking_key(event.key)
 
-
+    # print(myPlayer.cor,maze[myPlayer.cor[0]][myPlayer.cor[1]])
 
     screen.fill(WHITE)
     all_sprites_group.draw(screen)
